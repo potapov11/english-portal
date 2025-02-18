19937b83718e0a036141e57f12b854d4b1b3ca23
Serezha <volos11223344@yandex.ru>
2025-02-18 21:35:31 +0300

    добавил motion animation

--- src/components/MemoryFlip/MemoryFlipComponent/MemoryFlip.tsx
+++ src/components/MemoryFlip/MemoryFlipComponent/MemoryFlip.tsx
@@ -2,16 +2,33 @@ import React from 'react';
 import styles from './MemoryFlip.module.scss';
 import { FlipCard } from '../FlipCard/FlipCard';
 import classNames from 'classnames';
+import QueueAnim from 'rc-queue-anim';
 import { words } from '../../../utils/vars.js';
 import { IWordItem } from '../../../utils/types.ts';
+import { motion } from 'framer-motion';
+
+const itemVariants = {
+	hidden: { opacity: 0, y: 20 },
+	visible: { opacity: 1, y: 0 },
+};
+
+// Варианты анимации для контейнера
+const containerVariants = {
+	hidden: {}, // Начальное состояние контейнера
+	visible: {
+		transition: { staggerChildren: 0.05 }, // Задержка между дочерними элементами
+	},
+};
 
 export const MemoryFlip = () => {
 	return (
-		<div className={classNames(styles.container, styles.memory_wrap)}>
+		<motion.ul initial="hidden" animate="visible" variants={containerVariants} className={classNames(styles.container, styles.memory_wrap, 'queue-simple')}>
 			{words?.length > 0 &&
-				words.map((item: IWordItem) => {
-					return <FlipCard englishWord={item.english} russianWord={item.russian} key={item.id} />;
-				})}
-		</div>
+				words.map((item: IWordItem) => (
+					<motion.li key={item.id} variants={itemVariants} className="queue-item">
+						<FlipCard englishWord={item.english} russianWord={item.russian} />
+					</motion.li>
+				))}
+		</motion.ul>
 	);
 };
